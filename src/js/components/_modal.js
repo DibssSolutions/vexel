67f2e46cea63e5e34e868745b3d51edcf6845f79
Valerii Derevshchykov <kaktuz34@gmail.com>
2019-05-08 19:40:56 +0300

    upd modal

--- src/js/components/_modal.js
+++ src/js/components/_modal.js
@@ -5,10 +5,18 @@ const modal = () => {
 
   const controls = $('[data-modal-control]');
   const modals = $('[data-modal]');
+  let offsetTop;
+  let offsetLeft;
   BODY.on('click', '[data-modal-control]', function(e) {
     e.preventDefault();
     const control = $(this);
     const modal = modals.filter(`[data-modal="${control.data('modal-control')}"]`);
+    const modalInner = modal.find('[data-modal-container]');
+    offsetTop = control.offset().top - modalInner.height();
+    offsetLeft = control.offset().left - modalInner.width();
+    modalInner.css({
+      'transform': `scale(0.6) translate(${offsetLeft}px,${offsetTop}px)`
+    });
     if (!modal.hasClass(OPEN)) {
       modal.addClass(OPEN);
       HTML.css('paddingRight', SCROLL_WIDTH());
@@ -23,6 +31,7 @@ const modal = () => {
   BODY.on('click', '[data-modal]', function(e) {
     if ($(e.target).closest('[data-modal-inner]').length || $(e.target).closest('[data-modal-control]').length ) return;
     if ($(e.target).closest('[data-modal]').length) {
+      const modalInner = $(this).find('[data-modal-container]');
       $(this).removeClass(OPEN);
       if ($('[data-modal].is-open').length === 0) {
         HTMLBODY.removeClass(OVERFLOW_HIDDEN);
